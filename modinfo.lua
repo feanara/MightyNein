4cdfa06fd82228c9cef3b7ae12494f333225adf3
Feanara <emilykristen92@gmail.com>
2020-05-18 21:33:24 -0400

    artwork

--- modinfo.lua
+++ modinfo.lua
@@ -4,15 +4,14 @@ description = "Eight new characters"
 author = "Feanara"
 version = "0.1" -- This is the version of the template. Change it to your own number.
 
+-- This is the URL name of the mod's thread on the forum; the part after the ? and before the first & in the url
+forumthread = "/files/file/mighty-nein/"
+
 -- This lets other players know if your mod is out of date, update it to match the current version in the game
 api_version = 6
 
 dont_starve_compatible = true
-
 reign_of_giants_compatible = true
-
-hamlet_compatible = false
-
 
 icon_atlas = "modicon.xml"
 icon = "modicon.tex"
